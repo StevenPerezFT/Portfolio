10926498d97eecf16a0923282d63ac8ea02fde8f
Perez Steven <perezstevenft@gmail.com>
2024-06-17 15:25:32 -0600

    I do a change into the contact component

--- src/components/landing/contact-me/contact-me.tsx
+++ src/components/landing/contact-me/contact-me.tsx
@@ -6,15 +6,7 @@ function Contact() {
         <div className="w-full" id="contact-me" >
             <h2 className="text-porfolioBlue-100  text-2xl font-bold mb-4 ">Contact</h2>
             <section className="w-full pb-5 ">
-                <div className="justify-around grid grid-cols-1 md:flex">
-                    <span className="grid place-items-center">
-                        <p className="text-porfolioOrange-200 mb-4">
-                            Email
-                        </p>
-                        <div className="mb-3">
-                            <p>perezstevenft@gmail.com</p>
-                        </div>
-                    </span>
+                <div className="justify-around flex">
                     <span className="grid place-items-center mx-5">
                         <p className="pb-4 text-porfolioOrange-200">
                             LinkedIn
